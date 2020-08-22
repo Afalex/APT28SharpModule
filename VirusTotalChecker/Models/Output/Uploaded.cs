c46e8ed53a7148c24d7201dc38a95d0161084340
Afalex <34140310+Afalex@users.noreply.github.com>
2020-08-22 15:48:41 +0300

    Добавьте файлы проекта.

---
+++ VirusTotalChecker/Models/Output/Uploaded.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace VirusTotalChecker.Models.Output
+{
+    public class Uploaded
+    {
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+    /// <summary>
+    /// Uploaded file information
+    /// </summary>
+    public class Data
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
